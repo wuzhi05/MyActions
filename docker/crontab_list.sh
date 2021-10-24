0d123acd16bba943e67970fa71d50c0f8dc4678b
wuzhi05 <92559919+wuzhi05@users.noreply.github.com>
2021-10-24 13:35:54 +0800

    芥么做任务给芥么豆可以换Switch

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -64,6 +64,8 @@
 10 2,8 * * * node /scripts/jd_mf.js >> /scripts/logs/jd_mf.log 2>&1
 #芥么签到
 11 9,18 * * * node /scripts/jd_jmsign.js >> /scripts/logs/jd_jmsign.log 2>&1
+#芥么做任务
+15 9,18 * * * node /scripts/jd_jmtask.js >> /scripts/logs/jd_jmtask.log 2>&1
 #财富岛热气球接待
 30 * * * * node /scripts/jd_cfd_loop.js >> /scripts/logs/jd_cfd_loop.log 2>&1
 #jd_opencard4
@@ -171,7 +173,7 @@
 10 9-20/1 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
 # 京东摇一摇
 0 1,17 * * * node /scripts/jd_shake.js >> /scripts/logs/jd_shake.log 2>&1
-京东试用（默认注释，请配合取关脚本使用）
+#京东试用（默认注释，请配合取关脚本使用）
 10 0 * * *  node /scripts/jd_try_new.js >> /scripts/logs/jd_try_new.log 2>&1
 ##############默认注释活动##############
 # jd_cash_exchange
