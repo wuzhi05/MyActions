f6ff91410dc74af191d5720975f4b45703f381eb
wuzhi05 <92559919+wuzhi05@users.noreply.github.com>
2022-01-11 11:52:53 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -90,7 +90,18 @@
 0 0,10,20,21 * * * node /scripts/jd_nhRedEnvelope.js.js >> /scripts/logs/jd_nhRedEnvelope.js.log 2>&1
 #炸年兽
 33 0,6-23/2 * * * node /scripts/jd_travel.js.js >> /scripts/logs/jd_travel.js.log 2>&1
-
+#jd_opencard94
+30 1 5-25/2,26 1 * node /scripts/jd_opencard94.js >> /scripts/logs/jd_opencard94.log 2>&1
+#jd_opencard95
+30 2 1,11-31/3 1,2 * node /scripts/jd_opencard95.js >> /scripts/logs/jd_opencard95.log 2>&1
+#jd_opencard97
+30 2 28,7-28/3 1 * node /scripts/jd_opencard97.js >> /scripts/logs/jd_opencard97.log 2>&1
+#jd_opencard98
+30 10 27,7-27/3 1 * node /scripts/jd_opencard98.js >> /scripts/logs/jd_opencard98.log 2>&1
+#jd_opencard100
+30 3 9-15/3 1 * node /scripts/jd_opencard100.js >> /scripts/logs/jd_opencard100.log 2>&1
+#jd_opencard101
+30 2 27,9-26/3 1 * node /scripts/jd_opencard101.js >> /scripts/logs/jd_opencard101.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
