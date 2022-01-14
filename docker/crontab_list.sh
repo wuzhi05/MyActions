b47db1cf39bf40c4ec708d0804e142fd36a8e3a3
wuzhi05 <92559919+wuzhi05@users.noreply.github.com>
2022-01-14 23:10:25 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -109,7 +109,9 @@
 #饭粒
 10 9 * * * node /scripts/jd_fanli.js >> /scripts/logs/jd_fanli.log 2>&1
 #jd_opencard106
-30 4 14-21/3 1 * node /scripts/jd_opencard106.js >> /scripts/logs/jd_opencard106.log 2>&1
+30 6 14-21/3 1 * node /scripts/jd_opencard106.js >> /scripts/logs/jd_opencard106.log 2>&1
+#jd_opencard107
+30 0 14-21/3 1 * node /scripts/jd_opencard107.js >> /scripts/logs/jd_opencard107.log 2>&1
 ##############长期活动##############
 # 签到
 0 0,18 * * * cd /scripts && node jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
