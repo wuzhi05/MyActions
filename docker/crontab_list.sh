c0b63f2bc47ea25d810366e17f56f71336b53000
wuzhi05 <92559919+wuzhi05@users.noreply.github.com>
2022-02-14 12:42:18 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -114,8 +114,6 @@
 0 10 * * * node /scripts/jd_bean_change.js >> /scripts/logs/jd_bean_change.log 2>&1
 # 京豆变动简洁版
 0 9 * * * node /scripts/jd_bean_change_clean.js >> /scripts/logs/jd_bean_change_clean.log 2>&1
-# 天天提鹅
-18 * * * * node /scripts/jd_daily_egg.js >> /scripts/logs/jd_daily_egg.log 2>&1
 # 金融养猪
 12 * * * * node /scripts/jd_pigPet.js >> /scripts/logs/jd_pigPet.log 2>&1
 # 京喜工厂
