95766589bedf11dd61424450ef32c246ee71dce6
Shokovsky <55470217+vega0707@users.noreply.github.com>
2022-09-12 14:45:16 +0800

    Update jd_fruit.js
    update

--- jd_fruit.js
+++ jd_fruit.js
@@ -1310,11 +1310,11 @@ function readShareCode() {
       }}, async (err, resp, data) => {
       try {
         if (err) {
-          console.log(`${JSON.stringify(err)}`)
-          console.log(`${$.name} API请求失败，将切换为备用API`)
-          console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)
-          $.get({url: `https://raw.githubusercontent.com/shuyeshuye/RandomShareCode/main/JD_Fruit.json`, 'timeout': 10000},(err, resp, data)=>{
-          data = JSON.parse(data);})
+          //console.log(`${JSON.stringify(err)}`)
+          //console.log(`${$.name} API请求失败，将切换为备用API`)
+          //console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)
+          //$.get({url: `https://raw.githubusercontent.com/shuyeshuye/RandomShareCode/main/JD_Fruit.json`, 'timeout': 10000},(err, resp, data)=>{
+          //data = JSON.parse(data);})
         } else {
           if (data) {
             console.log(`随机取助力码放到您固定的互助码后面(不影响已有固定互助)`)
